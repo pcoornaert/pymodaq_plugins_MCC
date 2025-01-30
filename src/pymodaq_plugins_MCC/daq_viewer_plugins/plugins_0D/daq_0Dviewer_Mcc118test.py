342e1ba1bc98a8a084365c5f967cb72efdf7e5aa
pcoornaert <pcoornaert@gmail.com>
2025-01-31 00:52:23 +0100

    print all

--- src/pymodaq_plugins_MCC/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Mcc118test.py
+++ src/pymodaq_plugins_MCC/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Mcc118test.py
@@ -113,7 +113,7 @@ class DAQ_0DViewer_Mcc118test(DAQ_Viewer_base):
         except (HatError, ValueError) as err:
             print('\n', err)
 
-        print(self.settings['name'])
+        print(self.settings)
         
         self.controller.a_in_scan_start(channel_mask, samples_per_channel, self.settings['sample_rate'], options)
 
